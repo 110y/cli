5f058435994336ba942678f8e2f9c5165f577548
Federico G. Schwindt <fgsch@lodoss.net>
2022-06-01 11:02:31 +0100

    Hash main.wasm and not the package
    This moves the hash calculation into validate(), now validateAndHash(),
    to avoid opening the package twice.
    
    Fixes #243.

--- pkg/commands/compute/validate.go
+++ pkg/commands/compute/validate.go
@@ -1,6 +1,7 @@
 package compute
 
 import (
+	"crypto/sha512"
 	"fmt"
 	"io"
 	"os"
@@ -31,7 +32,7 @@ func (c *ValidateCommand) Exec(in io.Reader, out io.Writer) error {
 		return fmt.Errorf("error reading file path: %w", err)
 	}
 
-	if err := validate(p); err != nil {
+	if _, err = validateAndHash(p); err != nil {
 		c.Globals.ErrLog.AddWithContext(err, map[string]interface{}{
 			"Path": c.path,
 		})
@@ -48,24 +49,25 @@ type ValidateCommand struct {
 	path string
 }
 
-// validate is a utility function to determine whether a package is valid.
-// It attemptes to unarchive and read a tar.gz file from a specfic path,
+// validateAndHash is a utility function to determine whether a package is valid.
+// It attemptes to unarchive and read a tar.gz file from a specific path,
 // if successful, it then iterates through (streams) each file in the archive
 // checking the filename against a list of required files. If one of the files
 // doesn't exist it returns an error.
+// validateAndHash will also compute the SHA 512 for main.wasm.
 //
 // NOTE: This function is also called by the `deploy` command.
-func validate(path string) error {
+func validateAndHash(path string) (string, error) {
 	file, err := os.Open(filepath.Clean(path))
 	if err != nil {
-		return fmt.Errorf("error reading package: %w", err)
+		return "", fmt.Errorf("error reading package: %w", err)
 	}
 	defer file.Close() // #nosec G307
 
 	tar := archiver.NewTarGz()
 	err = tar.Open(file, 0)
 	if err != nil {
-		return fmt.Errorf("error unarchiving package: %w", err)
+		return "", fmt.Errorf("error unarchiving package: %w", err)
 	}
 	defer tar.Close()
 
@@ -74,13 +76,14 @@ func validate(path string) error {
 		"main.wasm":   false,
 	}
 
+	var hashSum string
 	for {
 		f, err := tar.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			return fmt.Errorf("error reading package: %w", err)
+			return "", fmt.Errorf("error reading package: %w", err)
 		}
 
 		for k := range files {
@@ -89,17 +92,28 @@ func validate(path string) error {
 			}
 		}
 
+		// We take advantage of having access to main.wasm's contents
+		// and compute the SHA 512 here. This is later used in pkgCompare.
+		if f.Name() == "main.wasm" {
+			h := sha512.New()
+			if _, err := io.Copy(h, f); err != nil {
+				f.Close()
+				return "", fmt.Errorf("error computing hashsum: %w", err)
+			}
+			hashSum = fmt.Sprintf("%x", h.Sum(nil))
+		}
+
 		err = f.Close()
 		if err != nil {
-			return fmt.Errorf("error closing package: %w", err)
+			return "", fmt.Errorf("error closing file: %w", err)
 		}
 	}
 
 	for k, found := range files {
 		if !found {
-			return fmt.Errorf("error validating package: package must contain a %s file", k)
+			return "", fmt.Errorf("error validating package: package must contain a %s file", k)
 		}
 	}
 
-	return nil
+	return hashSum, nil
 }
